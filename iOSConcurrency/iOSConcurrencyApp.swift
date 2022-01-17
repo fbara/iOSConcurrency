7097c28800ea09e39af8dc11f633f9b52ccf74e0
fbara <fbara1@yahoo.com>
2022-01-17 13:48:56 -0600

    Initial Commit

---
+++ iOSConcurrency/iOSConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyApp.swift
+//  iOSConcurrency
+//
+//  Created by Frank Bara on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
